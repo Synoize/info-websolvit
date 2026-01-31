2d86fea2500ef99ded622856382e8883ca6e50d6
synoize <suryavanshivam@gamil.com>
2026-01-31 18:31:56 +0530

    update

--- config.php
+++ config.php
@@ -33,9 +33,9 @@ define("ROOT_PATH", __DIR__);
 // Example result: http://localhost/websolvit
 $protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? "https://" : "http://";
 $host = $_SERVER['HTTP_HOST'] ?? "localhost";
-$projectFolder = "";
+// $projectFolder = "websolvit";
 
-define("BASE_URL", $protocol . $host . "/" . $projectFolder);
+define("BASE_URL", $protocol . $host . "/" . $host);
 
 // 5) COMMON HELPERS
 
