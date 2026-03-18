876aa7fc47af93c5761a364c12ef344d1f0ca8d3
samikshawararkar <wararkarsamiksha2003@gmail.com>
2026-03-18 18:11:36 +0530

    check previous commit

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     <!-- Icons -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+   
     <!-- Lucide Icons CDN -->
     <script src="https://unpkg.com/lucide@latest"></script>
 
